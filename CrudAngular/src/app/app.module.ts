30bbade95225675270794155ee42ab585f2cc34a
xeniaflores <xgrivas1@gmail.com>
2020-09-11 15:27:05 -0600

    Ejercicio Completo 1

---
+++ CrudAngular/src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+
+import { NgModule } from '@angular/core';
+import {FormsModule } from '@angular/forms';
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule, FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
